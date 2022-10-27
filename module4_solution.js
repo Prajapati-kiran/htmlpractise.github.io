399b3911833a49f1dbb3297b98fef557d3826c00
Prajapati-kiran <85058146+Prajapati-kiran@users.noreply.github.com>
2022-10-27 13:22:04 +0530

    Create module4_solution.js

---
+++ module4_solution.js
@@ -0,0 +1,9 @@
+var names = ["kiran","rahul","jack","david","John"];
+for(var i=0;i<names.length;i++){
+	if(names[i][0]=="j" || names[i][0]=="J"){
+		console.log("Goodbye j" + names[i]);
+	}
+	else{
+		console.log("hello " + names[i]);
+	}
+}
